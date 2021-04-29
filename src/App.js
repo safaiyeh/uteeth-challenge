5294c8f461e7a684da4f4bb68f5b28be17ca1ed8
Jason Safaiyeh <safaiyeh@protonmail.com>
2021-04-28 20:45:55 -0700

    Challenge code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+
+const SEARCH_ENDPOINT = (term) => `http://127.0.0.1:8080/?search=${term}`;
+function Search() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [results, setResults] = useState([]);
+  
+  useEffect(() => {
+    const filterAndSortResults = (data) =>{
+      const filteredData = data.filter(d => d.name.includes(searchTerm) || d.email === searchTerm);
+      filteredData.sort((a, b) => {
+        const aNameContainsTerm = a.name.split(' ').includes(searchTerm);
+        const bNameContainsTerm = b.name.split(' ').includes(searchTerm);
+
+        if (aNameContainsTerm && !bNameContainsTerm) return -1;
+        if (bNameContainsTerm && !aNameContainsTerm) return 1;
+
+        if (a.email === searchTerm) return -1;
+        if (b.email === searchTerm) return 1;
+
+        return 0;
+      });
+
+      return filteredData;
+    }
+
+    let requestValid = true;
+  
+    if (searchTerm.trim() !== '') {
+      fetch(SEARCH_ENDPOINT(searchTerm))
+      .then(response => response.json())
+      .then(data => {
+        if (requestValid) {
+          console.log(data)
+          setResults(filterAndSortResults(data))
+        }
+      });
+    } else if (requestValid) {
+      setResults([]);
+    }
+
+    return () => requestValid = false;
+  }, [searchTerm]);
+
+  return (
+    <>
+      <input value={searchTerm} onChange={(e) => setSearchTerm(e.currentTarget.value)} />
+      <Results results={results} />
+    </>
+  )
+}
+
+function Results({ results }) {
+  const [selected, setSelected] = useState('');
+
+  const onResultClick = (key) => {
+    if (selected === key) {
+      setSelected('')
+    } else {
+      setSelected(key)
+    }
+  }
+
+  useEffect(() => setSelected(''), [results]);
+  
+  return <>
+    {
+      results.map(result => (
+        <div key={result.id} style={{ backgroundColor: result.id === selected ? 'teal': 'white' }} onClick={() => onResultClick(result.id)}>
+          <h2>{result.name}</h2>
+          <p>{result.email}</p>
+        </div> 
+      ))
+    }
+  </>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search />
     </div>
   );
 }
